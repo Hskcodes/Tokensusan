5a6db5331781616c95c957b29b12dc291aaea1af
Hskcodes <hskdashing@aol.com>
2025-03-17 16:20:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -61,10 +61,10 @@ https://dashboard.shareus.io/
 # TRUE for yes FALSE if no 
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
 # only shareus service known rightnow rest you can test on your own
-SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "shortner.in")
-# SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "3e3c88f44d5911a0072eb36f6b15c02d58b13b22")
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "seturl.in")
+# SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "cd7c238960c49d5dc74f2c77b8a2fd06e915750d")
 #use this key if not working ☠️ (jokin!!)
-SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "3e3c88f44d5911a0072eb36f6b15c02d58b13b22")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "cd7c238960c49d5dc74f2c77b8a2fd06e915750d")
 #add your custom time in secs for shortlink expiration.
 # 24hr = 86400
 # 12hr = 43200
